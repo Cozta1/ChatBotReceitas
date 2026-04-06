29fc872024869fd2e468b3dd7651c648b01e29c8
Gustavo Lopes <gustavolopesmartins2506@gmail.com>
2026-04-06 00:47:17 -0300

    Correção de erro ao chamar gerar_resposta

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, jsonify
-from chatbot import gerar_resposta
+from flask import Flask, render_template, request, jsonify, session
+from chatbot import gerar_resposta, nova_sessao
 
 app = Flask(__name__)
+app.secret_key = 'segredo'  # necessário pro session
 
 @app.route('/')
 def home():
@@ -9,8 +10,20 @@ def home():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    mensagem = request.json['mensagem']
-    resposta = gerar_resposta(mensagem)
+    data = request.get_json()
+    mensagem = data.get('mensagem')
+
+    if 'chat_sessao' not in session:
+        session['chat_sessao'] = nova_sessao()
+
+    sessao = session['chat_sessao']
+
+    resposta = gerar_resposta(mensagem, sessao)
+
+    sessao["historico"].append((mensagem, resposta))
+
+    session['chat_sessao'] = sessao
+
     return jsonify({'resposta': resposta})
 
 if __name__ == '__main__':
